5db0c75047c7258216c8492ffb824ffe4ece7a40
yschindel <55532939+yschindel@users.noreply.github.com>
2023-02-06 15:49:33 +0100

    add docstring

--- fields.py
+++ fields.py
@@ -1,6 +1,9 @@
 
 
 class GenericField:
+    """
+    Generic data class.
+    """
     def __init__(self, name, value):
         self.name = str(name)
         self.value = str(value)
@@ -8,18 +11,42 @@ class GenericField:
 	
 
 class StringField(GenericField):
+    """
+    A class holding string information.
+    """
     def __init__(self, name, value):
+        """
+        Inits an new 'StringField' instance.
+
+        Args:
+            name (str): The field name
+            value (str): The filed value
+        """
         super().__init__(name, value)
         self.dataType = 'string'
 	
 
 class IntegerField(GenericField):
+    """
+    Inits an new 'IntegerField' instance.
+
+    Args:
+        name (str): The field name
+        value (str): The filed value
+    """
     def __init__(self, name, value):
         super().__init__(name, value)
         self.dataType = 'integer'
 	
 
 class FloatField(GenericField):
+    """
+    Inits an new 'FloatField' instance.
+
+    Args:
+        name (str): The field name
+        value (str): The filed value
+    """
     def __init__(self, name, value):
         super().__init__(name, value)
         self.dataType = 'float'
