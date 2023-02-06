785ad47a8ef9310a8d2d61127f30568ae9316912
yschindel <55532939+yschindel@users.noreply.github.com>
2023-02-06 15:42:22 +0100

    Update log.py

--- log.py
+++ log.py
@@ -9,11 +9,18 @@ class Log():
         logging.basicConfig(filename=os.path.join(baseDir, 'pyrectus.log'), level=logging.INFO,
                                 format='%(asctime)s %(levelname)s %(message)s')
 
-    def info(self, message):
-        logging.info(message)
+    def info(self, message=''):
+        if message:
+            logging.info(message)
 
-    def error(self, message):
-        logging.error(message)
+    def error(self, message=''):
+        if message:
+            logging.error(message)
 
-    def warning(self, message):
-        logging.warning(message)
+    def warning(self, message=''):
+        if message:
+            logging.warning(message)
+
+
+if __name__ == '__main__':
+    Log().info('test')
